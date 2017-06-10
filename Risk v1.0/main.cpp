209d2a6e5fb58f4314e2be61184097faa03ead95
JNestman <nes14004@byui.edu>
2017-06-10 15:01:36 -0600

    Initial Commit

---
+++ Risk v1.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Risk v1.0
+//
+//  Created by Josh Nestman on 6/10/17.
+//  Copyright © 2017 Josh Nestman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
